6299c23fda8a75f54b881d9be147987ec70685f0
AndreyTsvetkov <ts.andrey@gmail.com>
2014-07-03 20:21:41 +0400

    Added Dictionary<T>.Lookup(TKey key) : Maybe<T>

---
+++ MaybeDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Functional.Maybe
+{
+	public static class MaybeDictionary
+	{
+		/// <summary>
+		/// Tries to get value from Dictionary safely
+		/// </summary>
+		/// <typeparam name="TK"></typeparam>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="dictionary"></param>
+		/// <param name="key"></param>
+		/// <returns></returns>
+		public static Maybe<T> Lookup<TK, T>(this IDictionary<TK, T> dictionary, TK key)
+		{
+			var getter = MaybeFunctionalWrappers.Wrap<TK, T>(dictionary.TryGetValue);
+			return getter(key);
+		}
+	}
+}
