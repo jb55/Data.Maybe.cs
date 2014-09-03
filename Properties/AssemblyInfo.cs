43186b19c417477d8173b0ce51dfe69e2e7f4ed2
AndreyTsvetkov <ts.andrey@gmail.com>
2014-09-03 18:35:06 +0400

    new package version created

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Data.Maybe")]
+[assembly: AssemblyTitle("Functional.Maybe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bill Casarin&Andrey Tsvetkov")]
-[assembly: AssemblyProduct("Data.Maybe")]
-[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyProduct("Functional.Maybe")]
+[assembly: AssemblyCopyright("MIT, MEPhI")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -24,5 +24,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
