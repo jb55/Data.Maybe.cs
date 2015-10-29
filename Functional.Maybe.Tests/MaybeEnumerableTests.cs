09aaeb760a0d5d21d79fa189276e589b19a08fb4
sholland <holland_s91@yahoo.com>
2015-10-29 18:01:57 -0500

    Add for WhereValueExist method
    This method doesn't work. Evidently, there is an infinite loop.

---
+++ Functional.Maybe.Tests/MaybeEnumerableTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Functional.Maybe.Tests
+{
+    [TestClass]
+    public class MaybeEnumerableTests
+    {
+        [TestMethod]
+        public void WhereValueExist_Should_remove_Nothing_values()
+        {
+            var sequence = new Maybe<int>[] { 1.ToMaybe(), Maybe<int>.Nothing, 2.ToMaybe() };
+            int[] expected = { 1, 2 };
+
+            var actual = sequence.WhereValueExist().ToArray();
+
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
